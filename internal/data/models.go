c882bfabd4bffaad92285840a9f62a432e5f6225
kegodo <2018117874@ub.edu.bz>
2022-11-13 15:02:32 -0600

    Added components to the models.go file

--- internal/data/models.go
+++ internal/data/models.go
@@ -1,2 +1,24 @@
 // File: forum/internal/data/models.go
 package data
+
+import (
+	"database/sql"
+	"errors"
+)
+
+var (
+	ErrRecordNotFound = errors.New("record not found")
+	ErrEditConflict   = errors.New("edit conflict")
+)
+
+// A wrapper for out data models
+type Models struct {
+	Forums ForumModel
+}
+
+// NewModels() allows us to create a new model
+func NewModels(db *sql.DB) Models {
+	return Models{
+		Forums: ForumModel{DB: db},
+	}
+}

287c3e4ee186ecf6490e4e5c1e6f15d07115d645
kegodo <2018117874@ub.edu.bz>
2022-11-13 15:39:42 -0600

    Completed handlers.go and validator.go with components

--- cmd/api/handlers.go
+++ cmd/api/handlers.go
@@ -2,61 +2,256 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"net/http"
+
+	"forum.kevin.net/internal/data"
+	"forum.kevin.net/internal/validator"
 )
 
-func (app *application) logError(r *http.Request, err error) {
-	app.logger.Println(err)
-}
+func (app *application) createForumHandler(w http.ResponseWriter, r *http.Request) {
+	//Our target decode destination
+	var input struct {
+		Title       string `json:"title"`
+		Category    string `json:"category"`
+		Description string `json:"description"`
+		Publisher   string `json:"publisher"`
+		ReleaseDate int    `json:"releasedate"`
+	}
 
-// To send JSON-formatted error message
-func (app *application) errorResponse(w http.ResponseWriter, r *http.Request, status int, message interface{}) {
-	env := envelope{"error": message}
-	err := app.writeJSON(w, status, env, nil)
+	//Initialize a new json.Decoder instance
+	err := app.readJSON(w, r, &input)
 	if err != nil {
-		app.logError(r, err)
-		w.WriteHeader(http.StatusInternalServerError)
+		app.badRequestResponse(w, r, err)
+		return
+	}
+
+	//coping the valeus from the input struct to the new todo struct
+	forum := &data.Forum{
+		Title:       input.Title,
+		Category:    input.Category,
+		Description: input.Description,
+		Publisher:   input.Publisher,
+		ReleaseDate: input.ReleaseDate,
+	}
+
+	//Initialize a new Validator Instance
+	v := validator.New()
+
+	//check the map to determine if ther were any validation errors
+	if data.ValidateForum(v, forum); !v.Valid() {
+		app.failedValidationResponse(w, r, v.Errors)
+		return
+	}
+
+	//Creating a todo element
+	err = app.models.Forums.Insert(forum)
+	if err != nil {
+		app.serverErrorResponse(w, r, err)
+	}
+
+	//Create a location header for the newly created resource
+	headers := make(http.Header)
+	headers.Set("Location", fmt.Sprintf("/v1/forums/%d", forum.ID))
+
+	err = app.writeJSON(w, http.StatusCreated, envelope{"forum": forum}, headers)
+	if err != nil {
+		app.serverErrorResponse(w, r, err)
 	}
 }
 
-// Server error response
-func (app *application) serverErrorResponse(w http.ResponseWriter, r *http.Request, err error) {
-	//We log the error
-	app.logError(r, err)
+// The showentry handler will display an individual todo element
+func (app *application) showForumHandler(w http.ResponseWriter, r *http.Request) {
+	//getting the request data from param function
+	id, err := app.readIDParam(r)
+	if err != nil {
+		app.notFoundReponse(w, r)
+		return
+	}
 
-	//Prepare a message with the error
-	message := "the server encountered a problem and could not process the request"
-	app.errorResponse(w, r, http.StatusInternalServerError, message)
+	//Fetching the specific todo element
+	forum, err := app.models.Forums.Get(id)
+
+	//Handling errors
+	if err != nil {
+		switch {
+		case errors.Is(err, data.ErrRecordNotFound):
+			app.notFoundReponse(w, r)
+		default:
+			app.serverErrorResponse(w, r, err)
+		}
+		return
+	}
+
+	//Writing the data from the returned get()
+	err = app.writeJSON(w, http.StatusOK, envelope{"forum": forum}, nil)
+	if err != nil {
+		app.serverErrorResponse(w, r, err)
+	}
 }
 
-// The not found response
-func (app *application) notFoundReponse(w http.ResponseWriter, r *http.Request) {
-	//Create our message
-	message := "the requested resource could not be found"
-	app.errorResponse(w, r, http.StatusNotFound, message)
+// Facilitates an update action to the todo element in the database
+func (app *application) updateForumHandler(w http.ResponseWriter, r *http.Request) {
+	id, err := app.readIDParam(r)
+	if err != nil {
+		app.notFoundReponse(w, r)
+		return
+	}
+
+	//Fetch the original record from the database
+	forum, err := app.models.Forums.Get(id)
+
+	//Handling the errors
+	if err != nil {
+		switch {
+		case errors.Is(err, data.ErrRecordNotFound):
+			app.notFoundReponse(w, r)
+		default:
+			app.serverErrorResponse(w, r, err)
+		}
+		return
+	}
+
+	//Creating an input struct to hold data read in from the client
+	//Updating the input struct to use pointers because pointers have a default value of nil
+	var input struct {
+		Title       *string `json:"title"`
+		Category    *string `json:"category"`
+		Description *string `json:"description"`
+		Publisher   *string `json:"publisher"`
+		ReleaseDate *int    `json:"releasedate"`
+	}
+
+	//Initilizing a new json.Decoder instance
+	err = app.readJSON(w, r, &input)
+	if err != nil {
+		app.badRequestResponse(w, r, err)
+		return
+	}
+
+	//checking for any updates
+	if input.Title != nil {
+		forum.Title = *input.Title
+	}
+
+	if input.Title != nil {
+		forum.Category = *input.Category
+	}
+
+	if input.Description != nil {
+		forum.Description = *input.Description
+	}
+
+	if input.Publisher != nil {
+		forum.Publisher = *input.Publisher
+	}
+
+	if input.ReleaseDate != nil {
+		forum.ReleaseDate = *input.ReleaseDate
+	}
+
+	//Initilize a new Validator Instance
+	v := validator.New()
+
+	//Checking the map to determin if there were any validation errors
+	if data.ValidateForum(v, forum); !v.Valid() {
+		app.failedValidationResponse(w, r, v.Errors)
+		return
+	}
+
+	//Passing the updated todo element to the update() method
+	err = app.models.Forums.Update(forum)
+	if err != nil {
+		switch {
+		case errors.Is(err, data.ErrEditConflict):
+			app.editConflictResponse(w, r)
+		default:
+			app.serverErrorResponse(w, r, err)
+		}
+		return
+	}
+
+	//Writing the data returned by Get()
+	err = app.writeJSON(w, http.StatusOK, envelope{"forum": forum}, nil)
+	if err != nil {
+		app.serverErrorResponse(w, r, err)
+	}
 }
 
-// A method not allowed response
-func (app *application) methodNotAllowedResponse(w http.ResponseWriter, r *http.Request) {
-	//Create our message
-	message := fmt.Sprintf("the %s method is not supported for this resource", r.Method)
-	app.errorResponse(w, r, http.StatusInternalServerError, message)
+// To facilitate deletion of a todo element
+func (app *application) deleteForumHandler(w http.ResponseWriter, r *http.Request) {
+	id, err := app.readIDParam(r)
+	if err != nil {
+		app.notFoundReponse(w, r)
+		return
+	}
+
+	err = app.models.Forums.Delete(id)
+
+	if err != nil {
+		switch {
+		case errors.Is(err, data.ErrRecordNotFound):
+			app.notFoundReponse(w, r)
+		default:
+			app.serverErrorResponse(w, r, err)
+		}
+		return
+	}
+
+	//Returning 200 status ok to the client with a success message
+	err = app.writeJSON(w, http.StatusOK, envelope{"message": "forum element sucessfully deleted"}, nil)
+	if err != nil {
+		app.serverErrorResponse(w, r, err)
+	}
 }
 
-// User provided a bad request
-func (app *application) badRequestResponse(w http.ResponseWriter, r *http.Request, err error) {
-	//Create our message
-	app.errorResponse(w, r, http.StatusBadRequest, err.Error())
-}
+// The listTodo handler allows the client to see a listing of todo elements based on a set of criteria
+func (app *application) listForumHandler(w http.ResponseWriter, r *http.Request) {
+	//creating an input struct to hold our query parameters
+	var input struct {
+		Title       string
+		Category    string
+		Description string
+		data.Filters
+	}
 
-// Validation error
-func (app *application) failedValidationResponse(w http.ResponseWriter, r *http.Request, errors map[string]string) {
-	app.errorResponse(w, r, http.StatusUnprocessableEntity, errors)
-}
+	//Initializing a validator
+	v := validator.New()
 
-// Edit conflict error
-func (app *application) editConflictResponse(w http.ResponseWriter, r *http.Request) {
-	message := "unable to update the record due to an edit conflict, please try again"
-	app.errorResponse(w, r, http.StatusMethodNotAllowed, message)
+	//getting the URL values map
+	qs := r.URL.Query()
+
+	//Using the helper method to extract the values
+	input.Title = app.readString(qs, "title", "")
+	input.Category = app.readString(qs, "category", "")
+	input.Description = app.readString(qs, "decription", "")
+
+	//Get the page information
+	input.Filters.Page = app.readInt(qs, "page", 1, v)
+	input.Filters.PageSize = app.readInt(qs, "page_size", 20, v)
+	//Get the sort information
+	input.Filters.Sort = app.readString(qs, "sort", "id")
+	// Specific the allowed sort values
+	input.Filters.SortList = []string{"id", "title", "cateogry", "description", "-id", "-title", "-category", "-description"}
+
+	//checking for validation errors
+	if data.ValidateFilter(v, input.Filters); !v.Valid() {
+		app.failedValidationResponse(w, r, v.Errors)
+		return
+	}
+
+	//Geting a listing of all todo elements
+	forums, metadata, err := app.models.Forums.GetAll(input.Title, input.Category, input.Description, input.Filters)
+	if err != nil {
+		app.serverErrorResponse(w, r, err)
+		return
+	}
+
+	//sending JSON response
+	err = app.writeJSON(w, http.StatusOK, envelope{"forums": forums, "metadata": metadata}, nil)
+	if err != nil {
+		app.serverErrorResponse(w, r, err)
+		return
+	}
 }
--- internal/data/forum.go
+++ internal/data/forum.go
@@ -1,2 +1,246 @@
 // File: forum/internal/data/forum.go
 package data
+
+import (
+	"context"
+	"database/sql"
+	"errors"
+	"fmt"
+	"time"
+
+	"forum.kevin.net/internal/validator"
+)
+
+// Todo struct supports the infromation for the todo todo
+type Forum struct {
+	ID          int64     `json:"id"`
+	CreatedAt   time.Time `json:"-"`
+	Title       string    `json:"title"`
+	Category    string    `json:"category"`
+	Description string    `json:"description"`
+	Publisher   string    `json:"publisher"`
+	ReleaseDate int       `json:"releasedate"`
+	Version     int32     `json:"version"`
+}
+
+func ValidateForum(v *validator.Validator, forum *Forum) {
+	//using check() method to check our validation checks
+	v.Check(forum.Title != "", "title", "must be provided")
+	v.Check(len(forum.Title) <= 200, "title", "must not be more than 200 bytes long")
+
+	v.Check(forum.Category != "", "category", "must be provided")
+	v.Check(len(forum.Title) <= 250, "category", "must not be more than 250 bytes long")
+
+	v.Check(forum.Description != "", "description", "must be provided")
+	v.Check(len(forum.Description) <= 500, "description", "must not be more than 500 bytes long")
+
+	v.Check(forum.Publisher != "", "Publisher", "must be provided")
+	v.Check(len(forum.Publisher) <= 200, "Publisher", "must not be more than 200 bytes long")
+
+}
+
+type ForumModel struct {
+	DB *sql.DB
+}
+
+// Insert() allows us to create a new todo
+func (m ForumModel) Insert(forum *Forum) error {
+	query := `
+		INSERT INTO forums (title, category, description, publisher, releasedate)
+		VALUES ($1, $2, $3, $4, $5)
+		RETURNING id, createdat, version
+	`
+	//collect the date field into a slice
+	args := []interface{}{forum.Title, forum.Category, forum.Description, forum.Publisher, forum.ReleaseDate}
+	//creating the context
+	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+	//Clean up to prevent memory leaks
+	defer cancel()
+
+	return m.DB.QueryRowContext(ctx, query, args...).Scan(&forum.ID, &forum.CreatedAt, &forum.Version)
+}
+
+// Get() allows us to retrieve a specific task
+func (m ForumModel) Get(id int64) (*Forum, error) {
+	//Ensure that there is a valid id
+	if id < 1 {
+		return nil, ErrRecordNotFound
+	}
+
+	//Construct our query with the given id
+	query := `
+		SELECT id, createdat, title, category, description, publisher, releasedate, version
+		FROM forums
+		WHERE id = $1
+	`
+
+	//Declaring the Todo varaible to hold the returned data
+	var forum Forum
+
+	//Creating the context
+	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+	//Cleaning up to prevent memory leaks
+	defer cancel()
+
+	err := m.DB.QueryRowContext(ctx, query, id).Scan(
+		&forum.ID,
+		&forum.CreatedAt,
+		&forum.Title,
+		&forum.Category,
+		&forum.Description,
+		&forum.Publisher,
+		&forum.ReleaseDate,
+		&forum.Version,
+	)
+
+	if err != nil {
+		//Check the type of error
+		switch {
+		case errors.Is(err, sql.ErrNoRows):
+			return nil, ErrRecordNotFound
+		default:
+			return nil, err
+		}
+	}
+	//Succes
+	return &forum, nil
+}
+
+// Update() allows us to edit/alter a specific todo task
+// Optimistic locking (version number)
+func (m ForumModel) Update(forum *Forum) error {
+	//create a query
+	query := `
+		UPDATE forums
+		SET title = $1, category = $2, description = $3, publisher = $4, releasedate = $5, version = version + 1
+		WHERE id = $6
+		AND version = $7
+		RETURNING version
+	`
+	args := []interface{}{
+		forum.Title,
+		forum.Category,
+		forum.Description,
+		forum.Publisher,
+		forum.ReleaseDate,
+		forum.ID,
+		forum.Version,
+	}
+
+	//Creating the context
+	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+	//Cleaning up to prevent memory leaks
+	defer cancel()
+
+	//Check for edit conflicts
+	err := m.DB.QueryRowContext(ctx, query, args...).Scan(&forum.Version)
+	if err != nil {
+		//Check the type of error
+		switch {
+		case errors.Is(err, sql.ErrNoRows):
+			return ErrEditConflict
+		default:
+			return err
+		}
+	}
+	//Succes
+	return nil
+
+}
+
+func (m ForumModel) Delete(id int64) error {
+	//Ensure that there is a valid id
+	if id < 1 {
+		return ErrRecordNotFound
+	}
+	//Create the delete query
+	query := `
+		DELETE FROM forums
+		WHERE id = $1
+	`
+
+	//creating the context
+	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+	//clearing up to prevent memory leaks
+	defer cancel()
+
+	//Execute the query
+	result, err := m.DB.ExecContext(ctx, query, id)
+	if err != nil {
+		return err
+	}
+
+	//Check how many rows were affected by the delete operation.
+	//We call the RowsAffected() method on the result variable
+	rowsAffected, err := result.RowsAffected()
+	if err != nil {
+		return err
+	}
+
+	//Check if no rows were affected
+	if rowsAffected == 0 {
+		return ErrRecordNotFound
+	}
+	return nil
+}
+
+func (m ForumModel) GetAll(title string, category string, description string, filters Filters) ([]*Forum, Metadata, error) {
+	//constructing the query
+	query := fmt.Sprintf(`
+		SELECT COUNT(*) OVER(),
+	    id, createdat, title, category, description, publisher, releasedate, version
+		FROM forums
+		WHERE (to_tsvector('simple', title) @@ plainto_tsquery('simple', $1) OR $1 = '')
+		AND (to_tsvector('simple', category) @@ plainto_tsquery('simple', $2) OR $2 = '')
+		AND (to_tsvector('simple', description) @@ plainto_tsquery('simple', $3) OR $3 = '')
+		ORDER BY %s %s, id ASC
+		LIMIT $4 OFFSET $5`, filters.sortColumn(), filters.sortOrder())
+
+	//creating the 3 second time out context
+	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+	defer cancel()
+
+	//Execute the query
+	args := []interface{}{title, category, description, filters.limit(), filters.offSet()}
+	rows, err := m.DB.QueryContext(ctx, query, args...)
+	if err != nil {
+		return nil, Metadata{}, err
+	}
+
+	//Closing the result set
+	defer rows.Close()
+	totalRecords := 0
+
+	//Initialize an empty slice to hold the task data
+	forums := []*Forum{}
+
+	//Iterate over the rows in the result set
+	for rows.Next() {
+		var forum Forum
+
+		//Scanning the valus from the row into the todo struct
+		err := rows.Scan(
+			&totalRecords,
+			&forum.ID,
+			&forum.CreatedAt,
+			&forum.Title,
+			&forum.Category,
+			&forum.Description,
+			&forum.Publisher,
+			&forum.ReleaseDate,
+			&forum.Version,
+		)
+		if err != nil {
+			return nil, Metadata{}, err
+		}
+		//Add the todo to our slice
+		forums = append(forums, &forum)
+	}
+	//checking for errors after looping through the result set
+	if err = rows.Err(); err != nil {
+		return nil, Metadata{}, err
+	}
+	metadata := calculateMetaData(totalRecords, filters.Page, filters.PageSize)
+	//returning the slice of todos
+	return forums, metadata, nil
+}

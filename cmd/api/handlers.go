5e0173b7107a950f28295346cb7c36206593b580
kegodo <2018117874@ub.edu.bz>
2022-11-13 14:53:05 -0600

    Added the routes in the routes.go file

--- cmd/api/handlers.go
+++ cmd/api/handlers.go
@@ -2,21 +2,61 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 )
 
-func (app *application) healthcheckHandler(w http.ResponseWriter, r *http.Request) {
-	// Create a map to hold our healthcheck data
-	data := envelope{
-		"status": "available",
-		"system_info": map[string]string{
-			"environment": app.config.env,
-			"version":     version,
-		},
-	}
-	err := app.writeJSON(w, http.StatusOK, data, nil)
+func (app *application) logError(r *http.Request, err error) {
+	app.logger.Println(err)
+}
+
+// To send JSON-formatted error message
+func (app *application) errorResponse(w http.ResponseWriter, r *http.Request, status int, message interface{}) {
+	env := envelope{"error": message}
+	err := app.writeJSON(w, status, env, nil)
 	if err != nil {
-		app.serverErrorResponse(w, r, err)
-		return
+		app.logError(r, err)
+		w.WriteHeader(http.StatusInternalServerError)
 	}
+}
+
+// Server error response
+func (app *application) serverErrorResponse(w http.ResponseWriter, r *http.Request, err error) {
+	//We log the error
+	app.logError(r, err)
+
+	//Prepare a message with the error
+	message := "the server encountered a problem and could not process the request"
+	app.errorResponse(w, r, http.StatusInternalServerError, message)
+}
+
+// The not found response
+func (app *application) notFoundReponse(w http.ResponseWriter, r *http.Request) {
+	//Create our message
+	message := "the requested resource could not be found"
+	app.errorResponse(w, r, http.StatusNotFound, message)
+}
+
+// A method not allowed response
+func (app *application) methodNotAllowedResponse(w http.ResponseWriter, r *http.Request) {
+	//Create our message
+	message := fmt.Sprintf("the %s method is not supported for this resource", r.Method)
+	app.errorResponse(w, r, http.StatusInternalServerError, message)
+}
+
+// User provided a bad request
+func (app *application) badRequestResponse(w http.ResponseWriter, r *http.Request, err error) {
+	//Create our message
+	app.errorResponse(w, r, http.StatusBadRequest, err.Error())
+}
+
+// Validation error
+func (app *application) failedValidationResponse(w http.ResponseWriter, r *http.Request, errors map[string]string) {
+	app.errorResponse(w, r, http.StatusUnprocessableEntity, errors)
+}
+
+// Edit conflict error
+func (app *application) editConflictResponse(w http.ResponseWriter, r *http.Request) {
+	message := "unable to update the record due to an edit conflict, please try again"
+	app.errorResponse(w, r, http.StatusMethodNotAllowed, message)
 }
--- cmd/api/routes.go
+++ cmd/api/routes.go
@@ -1,2 +1,25 @@
 // File: forum/cmd/api/routes.go
 package main
+
+import (
+	"net/http"
+
+	"github.com/julienschmidt/httprouter"
+)
+
+func (app *application) routes() *httprouter.Router {
+	router := httprouter.New()
+
+	//security routes
+	router.NotFound = http.HandlerFunc(app.notFoundReponse)
+	router.MethodNotAllowed = http.HandlerFunc(app.methodNotAllowedResponse)
+
+	router.HandlerFunc(http.MethodGet, "/v1/healthcheck", app.healthcheckHandler)
+	router.HandlerFunc(http.MethodGet, "/v1/forum", app.listForumHandler)
+	router.HandlerFunc(http.MethodPost, "/v1/forum", app.createForumHandler)
+	router.HandlerFunc(http.MethodGet, "/v1/forum/:id", app.showForumHandler)
+	router.HandlerFunc(http.MethodPatch, "/v1/forum/:id", app.updateForumHandler)
+	router.HandlerFunc(http.MethodDelete, "/v1/forum/:id", app.deleteForumHandler)
+
+	return router
+}
